c3bf7bb2bc728245fe456bcb211a2f2a1324165b
Jon Su <jon@wisesync.ai>
2025-04-16 12:25:15 +0800

    Asssemble AppConfig.ServiceUrl dynamically

--- src/app_config.js
+++ src/app_config.js
@@ -1,7 +1,15 @@
 'use strict'
 
+const PORT = 8080;
+const url = new URL(window.location.href);
+let BaseUrl = '';
+if (url.hostname === 'localhost') {
+    BaseUrl = `http://localhost:${PORT}`;
+} else {
+    BaseUrl = `${url.protocol}//${url.hostname}:${BasePort}`;
+}
 export default class AppConfig {
     static ASRServiceUrl= 'ws://localhost:8080';
     static LLMServiceUrl= "http://114.34.190.146:8081";
-    static ServiceUrl= "http://localhost:8080";
+    static ServiceUrl= BaseUrl;
 }
