d272ba3dcfb1a646b041fbe813790dd2e63662b0
Jon Su <jon@wisesync.ai>
2025-05-09 09:52:00 +0800

    Enable cspNonce

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   ],
    // 啟用 CSP nonce 功能
    html: {
-    //cspNonce: 'OZRHt3ob7FPvfReQvpym2A=='
+    cspNonce: 'OZRHt3ob7FPvfReQvpym2A=='
   },
   resolve: {
     alias: {
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   server: {
     headers: {
-      'Content-Security-Policy':"default-src 'self';script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';font-src 'self' data:; img-src 'self' https://img.youtube.com/; frame-src 'self' https://www.youtube.com/; form-action 'none';connect-src 'self' http://localhost:8081 http://114.34.190.146:8081 http://localhost:8080 http://114.34.190.146:8080; frame-ancestors 'none'; base-uri 'self';",
+      'Content-Security-Policy':"default-src 'self';script-src 'self' 'OZRHt3ob7FPvfReQvpym2A=='; style-src 'self' 'unsafe-inline';font-src 'self' data:; img-src 'self' https://img.youtube.com/; frame-src 'self' https://www.youtube.com/; form-action 'none';connect-src 'self' http://localhost:8081 http://114.34.190.146:8081 http://localhost:8080 http://114.34.190.146:8080; frame-ancestors 'none'; base-uri 'self';",
       'X-Content-Type-Options': 'nosniff'
     }
 }
